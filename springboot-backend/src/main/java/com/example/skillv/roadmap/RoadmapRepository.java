47d3ebd46192d5c1b01a925110af458cc7f0651a
bishna shohani <bishnashoha11@gmail.com>
2025-04-28 15:40:54 +0530

    Import Package

--- springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapRepository.java
+++ springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// import an package
+
 @Repository
 public interface RoadmapRepository extends MongoRepository<Roadmap, String> {
 
