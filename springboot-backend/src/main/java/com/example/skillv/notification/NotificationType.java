c5f67a7869dff2f8ca6b632f9a3b6f7f22991e62
bishna shohani <bishnashoha11@gmail.com>
2025-05-01 22:28:01 +0530

    change notification ID

--- springboot-backend/src/main/java/com/example/skillv/notification/NotificationType.java
+++ springboot-backend/src/main/java/com/example/skillv/notification/NotificationType.java
@@ -1,5 +1,5 @@
 package com.example.skillv.notification;
-
+//
 public enum NotificationType {
     COMMENT,
     LIKE,
--- springboot-backend/src/main/java/com/example/skillv/roadmap/Roadmap.java
+++ springboot-backend/src/main/java/com/example/skillv/roadmap/Roadmap.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 @Document("roadmaps")
 @Data
 @AllArgsConstructor
