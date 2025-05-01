ccc2146fe3136573a1e3ac65bb8d0daf0451e5a9
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-02 00:59:39 +0530

    change the code in comments

--- springboot-backend/src/main/java/com/example/skillv/notification/Notification.java
+++ springboot-backend/src/main/java/com/example/skillv/notification/Notification.java
@@ -9,7 +9,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-
+//changed 
 @Document("notifications")
 @Data
 @AllArgsConstructor
