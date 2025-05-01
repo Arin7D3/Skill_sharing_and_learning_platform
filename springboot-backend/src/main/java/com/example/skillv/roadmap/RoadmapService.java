f9b5147e4c2c548c5cfc6c11ace1189d89916f3e
bishna shohani <bishnashoha11@gmail.com>
2025-05-01 22:17:44 +0530

    Added road map operations

--- springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapService.java
+++ springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-// services
+// services can be 
 @Service
 @RequiredArgsConstructor
 public class RoadmapService {
