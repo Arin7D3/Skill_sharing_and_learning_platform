cd315d7604638db0bd5a500c37d252c6130dfe38
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-02 00:42:39 +0530

    created authcontroller

--- springboot-backend/src/main/java/com/example/skillv/auth/AuthController.java
+++ springboot-backend/src/main/java/com/example/skillv/auth/AuthController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//auth
 import java.io.IOException;
 
 @RestController
