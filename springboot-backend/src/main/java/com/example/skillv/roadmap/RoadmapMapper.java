1b994bfe900725378a5726897f08808136e4c57c
bishna shohani <bishnashoha11@gmail.com>
2025-05-01 22:05:25 +0530

    Added new component

--- springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapMapper.java
+++ springboot-backend/src/main/java/com/example/skillv/roadmap/RoadmapMapper.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-//deleted
+// Added new component
 @Component
 @RequiredArgsConstructor
 public class RoadmapMapper{
