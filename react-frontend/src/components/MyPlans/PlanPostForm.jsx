295153213d948a3866c5de9cddadbdc4f27f14f8
bishna shohani <bishnashoha11@gmail.com>
2025-05-02 04:01:52 +0530

    form

--- react-frontend/src/components/MyPlans/PlanPostForm.jsx
+++ react-frontend/src/components/MyPlans/PlanPostForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { planService } from '../../services/planService';
 import { useNotification } from '../../components/Notification/NotificationContext';
-
+//form
 export default function PlanPostForm() {
   const [formData, setFormData] = useState({
     title: '',
