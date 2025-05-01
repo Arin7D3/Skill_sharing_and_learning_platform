ecacdd445272bfaeb21884e49bf5e46d3ff89636
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-02 00:50:35 +0530

    updated user.java

--- springboot-backend/src/main/java/com/example/skillv/user/User.java
+++ springboot-backend/src/main/java/com/example/skillv/user/User.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+//err
 import java.time.LocalDateTime;
 import java.util.List;
 
